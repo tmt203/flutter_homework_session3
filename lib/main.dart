592003b8bf97cc1cf4a4f28d26fcdfcb019035d3
Tran Minh Tri <tranminhtri0253@gmail.com>
2022-06-06 23:13:36 +0700

    Main interface completed

--- lib/main.dart
+++ lib/main.dart
@@ -28,12 +28,31 @@ const textButtonFont = TextStyle(
   height: (16.8 / 14), // line height = fontsize * height
 );
 
+const titleAccountStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+  wordSpacing: 0.03,
+  height: (45 / 30), // line height = fontsize * height
+);
+
+const subAccountStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+  wordSpacing: 0.03,
+  height: (45 / 30), // line height = fontsize * height
+);
+
 // context
-const titleText = "Enjoy The New\nArrival Product";
+const titleText1 = "Enjoy The New Arrival Product";
+const titleText2 = "Guaranteed Safe Delivery";
+const titleText3 = "Goods Arrived\n On Time";
 const subTitleText =
     "Get your dream item easily and safely with\n Shoesly. and get other interesting offers";
 
 // icons path
+const mainImagePath1 = "assets/images/hinh1.png";
+const mainImagePath2 = "assets/images/hinh2.png";
+const mainImagePath3 = "assets/images/hinh3.png";
 const googleIconPath = "assets/icons/google.png";
 const fbIconPath = "assets/icons/facebook.png";
 
@@ -51,14 +70,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      // home: const MyHomePage(
+      //   imagePath: mainImagePath3, // mainImagePath2 mainImagePath3
+      //   title: titleText3, // titleText2 titleText3
+      //   subTitle: subTitleText,
+      // ),
+      home: const SignIn(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+// Main interface 1
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({
+    Key? key,
+    required this.imagePath,
+    required this.title,
+    required this.subTitle,
+  }) : super(key: key);
+
+  final String imagePath;
+  final String title;
+  final String subTitle;
 
   @override
   Widget build(BuildContext context) {
@@ -73,20 +107,20 @@ class MyHomePage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 38),
               width: 300,
               height: 300,
-              child: Image.asset("assets/images/hinh1.png"),
+              child: Image.asset(imagePath),
             ),
 
             // Main title
-            //const SizedBox(height: 33),
+            const SizedBox(height: 33),
             Column(
-              children: const [
-                MainTitle(titleText: titleText, titleFont: titleFont),
-                SizedBox(height: 20),
-                MainTitle(titleText: subTitleText, titleFont: subTitleFont),
+              children: [
+                MainTitle(titleText: title, titleFont: titleFont),
+                const SizedBox(height: 20),
+                MainTitle(titleText: subTitle, titleFont: subTitleFont),
               ],
             ),
             // Slash (not completed yet)
-            const SizedBox(height: 50),
+            const SizedBox(height: 10),
 
             // Buttons
             const SizedBox(height: 57),
@@ -127,9 +161,38 @@ class MyHomePage extends StatelessWidget {
                       FootButton(iconPath: fbIconPath, text: "FACEBOOK"),
                     ],
                   ),
-                )
+                ),
+                const SizedBox(height: 32),
               ],
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Sign in interface
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 92),
+            RichText(
+              text: TextSpan(
+                style: GoogleFonts.urbanist(),
+                children: const [
+                  TextSpan(text: "Sign Up\n"),
+                  TextSpan(text: "Please sign up to your Shoesly Account")
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -151,25 +214,24 @@ class FootButton extends StatelessWidget {
     return Container(
       width: 150,
       height: 50,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100),
         border: Border.all(color: const Color(0xFFE7E7E7)),
       ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(iconPath),
-            const SizedBox(width: 8),
-            Text(
-              text,
-              style: GoogleFonts.urbanist(
-                textStyle: textButtonFont,
-                color: titleColor,
-              ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(iconPath),
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: GoogleFonts.urbanist(
+              textStyle: textButtonFont,
+              color: titleColor,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -187,16 +249,15 @@ class MainTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          titleText,
-          style: GoogleFonts.urbanist(
-            textStyle: titleFont,
-          ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(35, 0, 25, 0),
+      child: Text(
+        titleText,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.urbanist(
+          textStyle: titleFont,
         ),
-      ],
+      ),
     );
   }
 }
